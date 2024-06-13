41a0cd340ad844454035dcd2ee0a3cd281d1610b
lxiaofenglix <lxiaofenglix@gmail.com>
2024-06-14 00:08:25 +0800

    clean

--- socks5-server.py
+++ socks5-server.py
@@ -5,6 +5,20 @@ import threading
 
 LOCAL_IP = '0.0.0.0'
 
+# 定义 SOCKS5 协议相关常量
+SOCKS_VERSION = b'\x05'
+METHOD_NO_AUTH = b'\x00'
+METHOD_NOT_ACCEPTABLE = b'\xFF'
+CMD_CONNECT = 1
+CMD_UDP_ASSOCIATE = 3
+ADDR_TYPE_IPV4 = 1
+ADDR_TYPE_DOMAIN = 3
+ADDR_TYPE_IPV6 = 4
+REPLY_SUCCEEDED = b'\x00'
+REPLY_GENERAL_FAILURE = b'\x01'
+REPLY_COMMAND_NOT_SUPPORTED = b'\x07'
+REPLY_ADDR_TYPE_NOT_SUPPORTED = b'\x08'
+
 def handle_tcp_proxy(sock, dst_addr, dst_port):
     try:
         # TCP Step2: 连接到目标服务器，建立 TCP 链接
@@ -46,7 +60,6 @@ def handle_udp_proxy(sock, dst_addr, dst_port):
         udp_sock.bind((LOCAL_IP, 0))
         _, local_port = udp_sock.getsockname()
         ip_address = socket.gethostbyname(socket.gethostname())
-        print(ip_address)
         print(f'[Server UDP Step2] connect {LOCAL_IP}:{local_port}')
 
         # UDP Step3: 告诉客户端 UDP 端口已建立
@@ -99,7 +112,7 @@ def handle_tcp(sock):
         # | 1  |    1     | 1 to 255 |
         # +----+----------+----------+
         ver = sock.recv(1)
-        if ver != b'\x05':
+        if ver != SOCKS_VERSION:
             raise Exception(f'Unsupported Socks Version: {ver}')
         nmethods = sock.recv(1)[0]
         methods = sock.recv(nmethods)
@@ -111,7 +124,7 @@ def handle_tcp(sock):
         # +----+--------+
         # | 1  |   1    |
         # +----+--------+
-        sock.sendall(b'\x05\x00')  # 这里默认选择不验证(0x00), 其他验证方式需要额外实现
+        sock.sendall(SOCKS_VERSION+METHOD_NO_AUTH)  # 这里默认选择不验证(0x00), 其他验证方式需要额外实现
         print(f'[Server 协商 Step2]: VER=5, METHOD=0')
 
         # TCP Step1 / UDP Step1: 客户端请求连接
@@ -121,11 +134,12 @@ def handle_tcp(sock):
         # | 1  |  1  | X'00' |  1   | Variable |    2     |
         # +----+-----+-------+------+----------+----------+
         ver, cmd, rsv, atyp = sock.recv(4)
-        if cmd != 1 and cmd != 3:   # 仅支持 tcp 和 udp
+        if cmd not in (CMD_CONNECT, CMD_UDP_ASSOCIATE):   # 仅支持 tcp 和 udp
             raise Exception(f'[Client TCP Step1 / UDP Step1] Unsupported Request Types: {cmd}')
-        if atyp == 1:  # IPv4
+        
+        if atyp == ADDR_TYPE_IPV4:  # IPv4
             dst_addr = socket.inet_ntoa(sock.recv(4))
-        elif atyp == 3:  # Domain name
+        elif atyp == ADDR_TYPE_IPV6:  # Domain name
             domain_len = sock.recv(1)[0]
             dst_addr = sock.recv(domain_len)
         else:
@@ -133,44 +147,15 @@ def handle_tcp(sock):
         dst_port = struct.unpack('>H', sock.recv(2))[0]
         print(f'[Client TCP Step1 / UDP Step1]: VER={ver}, CMD={cmd}, RSV={rsv}, ATYP={atyp}, DST.ADDR={dst_addr}, DST.PORT={dst_port}')
 
-        if cmd == 1:
+        if cmd == CMD_CONNECT:
             handle_tcp_proxy(sock, dst_addr, dst_port)
-        if cmd == 3:
+        if cmd == CMD_UDP_ASSOCIATE:
             handle_udp_proxy(sock, dst_addr, dst_port)
     except Exception as ex:
         print(ex)
     finally:
         sock.close()
-'''
-def handle_udp(sock, udp_sock):
-    while True:
-        data, addr = udp_sock.recvfrom(4096)
-        if data[0] != 0 or data[1] != 0 or data[2] != 0:
-            continue  # Invalid UDP request
 
-        # Extract the target address and port
-        addr_type = data[3]
-        if addr_type == 1:  # IPv4
-            target_addr = socket.inet_ntoa(data[4:8])
-            target_port = struct.unpack('>H', data[8:10])[0]
-            payload = data[10:]
-        elif addr_type == 3:  # Domain name
-            domain_len = data[4]
-            target_addr = data[5:5 + domain_len]
-            target_port = struct.unpack('>H', data[5 + domain_len:7 + domain_len])[0]
-            payload = data[7 + domain_len:]
-        else:
-            continue  # Unsupported address type
-
-        # Send the payload to the target
-        udp_sock.sendto(payload, (target_addr, target_port))
-
-        # Receive the response from the target
-        response, _ = udp_sock.recvfrom(4096)
-
-        # Send the response back to the client
-        udp_sock.sendto(b'\x00\x00\x00\x01' + socket.inet_aton(target_addr) + struct.pack('>H', target_port) + response, addr)
-'''
 def main():
     global LOCAL_IP
     tsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
